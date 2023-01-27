6de267305dc100bac2e10d85e33fef8073292133
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-26 20:36:45 -0800

    New Project

---
+++ src/main/java/com/rest/service/RestfulWebservice/UserController.java
@@ -0,0 +1,6 @@
+package com.rest.service.RestfulWebservice;
+
+public class UserController {
+
+
+}
---
+++ src/test/java/com/rest/service/RestfulWebservice/RestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.service.RestfulWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
