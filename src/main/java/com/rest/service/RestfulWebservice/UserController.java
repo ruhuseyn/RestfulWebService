321c3632c633da1eebcff8be7e6ea1c62c63d6a1
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-28 12:32:08 -0800

    ad pathvariable

--- src/main/java/com/rest/service/RestfulWebservice/UserController.java
+++ src/main/java/com/rest/service/RestfulWebservice/UserController.java
@@ -1,5 +1,7 @@
 package com.rest.service.RestfulWebservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 
-    public St
+    @GetMapping("/getuser/{id}")
+    public User getUser(@PathVariable String name){
+        return new User(String.format("Hello ",name));
+    }
 }
