f623ec1d6878463a95d021e5e41c79bc27a5e1b2
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-28 15:40:16 -0800

    add UserDaoService

---
+++ src/main/java/com/rest/service/RestfulWebservice/UserDaoSerice.java
@@ -0,0 +1,23 @@
+package com.rest.service.RestfulWebservice;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoSerice {
+
+    private static List<User> list = new ArrayList<>();
+
+    static{
+        list.add(new User(1,"Rufet", LocalDate.now().minusYears(30)));
+        list.add(new User(2,"Ugur", LocalDate.now().minusYears(25)));
+        list.add(new User(3,"Rauf", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> getAll(){
+        return list;
+    }
+}
