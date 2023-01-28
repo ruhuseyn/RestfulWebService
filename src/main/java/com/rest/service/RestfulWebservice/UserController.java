3ae197099cb18560663572e53b91f44a7ecc23b3
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-28 15:45:47 -0800

    add UserController

--- src/main/java/com/rest/service/RestfulWebservice/UserController.java
+++ src/main/java/com/rest/service/RestfulWebservice/UserController.java
@@ -1,16 +1,23 @@
 package com.rest.service.RestfulWebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @GetMapping("/getuser/{id}")
-    public User getUser(@PathVariable String name){
-        return new User(String.format("Hello,%s",name));
+    private final UserDaoSerice userDaoSerice;
+
+    public UserController(UserDaoSerice userDaoSerice) {
+        this.userDaoSerice = userDaoSerice;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoSerice.getAll();
     }
 }
