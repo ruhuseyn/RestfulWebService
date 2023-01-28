120246be17c5cc98cc1b501661b9dd7b7f29491f
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-28 12:26:43 -0800

    28/1

---
+++ src/main/java/com/rest/service/RestfulWebservice/UserController.java
@@ -0,0 +1,11 @@
+package com.rest.service.RestfulWebservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    public St
+}
