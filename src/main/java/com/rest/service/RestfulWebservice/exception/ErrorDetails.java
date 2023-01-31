09c7412a3fc3bd6252d43082b37a835879413b1a
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-30 22:10:54 -0800

    ErrorDetails

---
+++ src/main/java/com/rest/service/RestfulWebservice/exception/ErrorDetails.java
@@ -0,0 +1,28 @@
+package com.rest.service.RestfulWebservice.exception;
+
+import java.time.LocalDate;
+
+public class ErrorDetails {
+
+    private LocalDate timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDate timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
