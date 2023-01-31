e302cf1055a8908c7a668009c32aabde3322e699
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-30 22:29:08 -0800

    CustimezResponseEntityExceptionHnadler

--- src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -19,4 +19,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
         return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
 }
