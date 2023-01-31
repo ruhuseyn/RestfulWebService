4380e755803f21ad6b0d78d08752562ef4d8fbac
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-30 23:02:48 -0800

    Some Changes

--- src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.rest.service.RestfulWebservice.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,5 +28,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
