49cc16a3221b7be4ae3418d09899ca49a782e89b
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-30 22:27:13 -0800

    CustimezResponseEntityExceptionHnadler

--- src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/rest/service/RestfulWebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -8,13 +8,14 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),ex.getMessage(),request.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
 
         return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/rest/service/RestfulWebservice/exception/ErrorDetails.java
+++ src/main/java/com/rest/service/RestfulWebservice/exception/ErrorDetails.java
@@ -1,20 +1,21 @@
 package com.rest.service.RestfulWebservice.exception;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ErrorDetails {
 
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String details;
 
-    public ErrorDetails(LocalDate timestamp, String message, String details) {
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
         this.timestamp = timestamp;
         this.message = message;
         this.details = details;
     }
 
-    public LocalDate getTimestamp() {
+    public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
