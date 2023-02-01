b0ed4f41bea9052995beaa8a6c795fe69d456532
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-01 15:26:23 +0400

    Some Changes

--- src/main/java/com/rest/service/RestfulWebservice/controller/VersioningPersonController.java
+++ src/main/java/com/rest/service/RestfulWebservice/controller/VersioningPersonController.java
@@ -1,4 +1,13 @@
 package com.rest.service.RestfulWebservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VersioningPersonController {
+
+    @GetMapping("/v1/person")
+    public Person getFirstVersionOfPerson(){
+        return new Person("Rufet Huseynov");
+    }
 }
