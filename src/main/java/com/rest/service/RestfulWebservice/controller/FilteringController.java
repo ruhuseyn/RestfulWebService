afd9e31eda963ddb668b6ec9b8db04f105830430
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-11 19:59:11 +0400

    Some Changes

--- src/main/java/com/rest/service/RestfulWebservice/controller/FilteringController.java
+++ src/main/java/com/rest/service/RestfulWebservice/controller/FilteringController.java
@@ -1,5 +1,8 @@
 package com.rest.service.RestfulWebservice.controller;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.rest.service.RestfulWebservice.model.SomeBean;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +13,11 @@ public class FilteringController {
 
     @GetMapping("/filtering")
     public MappingJacksonValue getValue(){
-        SomeBean someBean = new SomeBean("value1","value2","value3")
-        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean)
+        SomeBean someBean = new SomeBean("value1","value2","value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("value1","value3");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mappingJacksonValue.setFilters(filterProvider);
         return mappingJacksonValue;
     }
 
