59d8951076a9dfc1b1184de740c107578cfcca8e
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-11 19:54:24 +0400

    Some Changes

--- src/main/java/com/rest/service/RestfulWebservice/controller/FilteringController.java
+++ src/main/java/com/rest/service/RestfulWebservice/controller/FilteringController.java
@@ -1,4 +1,22 @@
 package com.rest.service.RestfulWebservice.controller;
 
+import com.rest.service.RestfulWebservice.model.SomeBean;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FilteringController {
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue getValue(){
+        SomeBean someBean = new SomeBean("value1","value2","value3")
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean)
+        return mappingJacksonValue;
+    }
+
+    @GetMapping("/filtering-list")
+    public SomeBean getAllValue(){
+        return new SomeBean("v","v","v");
+    }
 }
