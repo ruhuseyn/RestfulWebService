ff820a141801fb42e138aab6ba806f48f3de4849
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-15 00:21:27 +0400

    Some Changes

---
+++ src/main/java/com/rest/service/RestfulWebservice/model/Post.java
@@ -0,0 +1,22 @@
+package com.rest.service.RestfulWebservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+}
