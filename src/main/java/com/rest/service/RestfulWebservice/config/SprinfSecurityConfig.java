3eb0d65bb8908f3e5b4cf764e4712a2b979bb972
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-18 22:43:46 +0400

    Some Changes

--- src/main/java/com/rest/service/RestfulWebservice/config/SprinfSecurityConfig.java
+++ src/main/java/com/rest/service/RestfulWebservice/config/SprinfSecurityConfig.java
@@ -2,6 +2,7 @@ package com.rest.service.RestfulWebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,7 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SprinfSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                auth-> auth.anyRequest().authenticated()
+        );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
         return httpSecurity.build();
     }
 
